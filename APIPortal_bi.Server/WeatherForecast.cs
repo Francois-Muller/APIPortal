228559688cac5f419a7c0bf737c01272d2960479
Francois Muller <francois.muller@qa-square.com>
2026-07-19 18:58:03 +0100

    Added solution projects

---
+++ APIPortal_bi.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace APIPortal_bi.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
